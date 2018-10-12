235aa2bf6d2116f4d6472e66f08a2793599d308c
Daniel Pereira <danielpp95@gmail.com>
2018-10-12 23:23:41 +0100

    user interface added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platzinger';
+  
+  
+  
+  constructor() {
+    
+    let myUser: User = {
+      nick: 'hola',
+      age: 12,
+      email: 'as',
+      uid: 2,
+      friend: false
+    }
+
+    console.log(myUser)
+  }
+
 }
