1bb5284d9b76e4b8e4d00f53d63c83da95e9b0c6
Daniel Pereira <danielpp95@gmail.com>
2018-10-12 22:47:20 +0100

    routes ready!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ConversationComponent } from './conversation/conversation.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'conversation', component: ConversationComponent },
+  { path: 'profile', component: ProfileComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    ConversationComponent
+    ConversationComponent,
+    ProfileComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
